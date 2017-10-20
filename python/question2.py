636b0b8c910829eae4a013de21a7d41fd7949bb8
Katherine Chen <cykatherinee@hotmail.com>
2017-10-20 11:17:03 +1100

    Implement basic cases for `decision_tree_learning`

--- python/question2.py
+++ python/question2.py
@@ -1,3 +1,5 @@
+import random
+
 def read_datafile(fname, attribute_data_type = 'integer'):
    inf = open(fname,'r')
    lines = inf.readlines()
@@ -21,21 +23,74 @@ def read_datafile(fname, attribute_data_type = 'integer'):
    return X, Y
 
 #===
+class Example:
+   def __init__(self, features, outcome):
+      self.features = features
+      self.outcome = outcome
+
+
 class DecisionTree :
    def __init__(self, split_random, depth_limit, curr_depth = 0, default_label = 1):
       self.split_random = split_random # if True splits randomly, otherwise splits based on information gain
       self.depth_limit = depth_limit
+      self.parent = None
+      self.default_label = default_label
+      self.current_depth = 0
 
+   def decision_tree_learning(self, examples, features_to_choose):
+      if self.current_depth >= self.depth_limit:
+         return self.plurality_value(examples)
 
+      if len(examples) == 0:
+         return self.default_label
+      elif self.check_same_classification(examples):
+         return examples[0].outcome
+      elif not features_to_choose:
+         return self.plurality_value(examples)
+      else:
+         if self.split_random:
+            next_feature = random.choice(features_to_choose)  # Choose a random feature
+         else:
+            next_feature = # TODO: Choice feature based on entropy
 
+   def check_same_classification(self, examples):
+      """ Check if all the samples have the same outcome. """
+      current = examples[0].outcome
+      for i in range(1, len(examples)):
+         if examples[i].outcome == current:
+            continue
+         else:
+            return False
+      return True
 
-   def train(self, X_train, Y_train) :
+   def plurality_value(self, examples):
+      """
+      Return the most common output value among examples.
+      When there is a tie, return randomly.
+      """
+      1_count = 0
+      0_count = 0
+      result = 0
+      for i in range(len(examples)):
+         if examples[i].outcome == 1:
+            1_count += 1
+         else:
+            0_count += 1
+      if 1_count > 0_count:
+         result = 1
+      elif 1_count == 0_count:
+         result = random.choice([0, 1])
+      return result
+
+   def train(self, X_train, Y_train):
       # receives a list of objects of type Example
       # TODO: implement decision tree training
+      pass
 
    def predict(self, X_train):
       # receives a list of booleans
       # TODO: implement decision tree prediction
+      print("Kaaaaat")
 
 #===
 def compute_accuracy(dt_classifier, X_test, Y_test):
